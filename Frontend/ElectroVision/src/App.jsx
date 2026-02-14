5b4454cdec010484bf7079d52820f262a32a537c
ArYaNDaNy <aryandany15@gmail.com>
2026-02-14 17:13:53 +0530

    enhanced website

--- Frontend/ElectroVision/src/App.jsx
+++ Frontend/ElectroVision/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home"; // 1. Import Home
 
 function App() {
   const handleLogin = (user) => console.log("Logged in:", user);
@@ -13,6 +14,10 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/Login" element={<Login onLogin={handleLogin} />} />
         <Route path="/signup" element={<Signup onSignup={handleSignup} />} />
+        
+        {/* 2. Add the Route for Home */}
+        {/* Make sure the path matches exactly what you wrote in navigate("/Home") */}
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </Router>
   );
